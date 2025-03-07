44cc039c6723fffe06c1a1fcef1550e3ef916184
Josue Ramirez <84062963+josueramirez1@users.noreply.github.com>
2025-03-07 14:52:02 -0800

    Add files via upload

---
+++ js-css-html-drag-and-drop/js/app.js
@@ -0,0 +1,25 @@
+const taskContainers = document.querySelectorAll(".task-container");
+const tasks = document.querySelectorAll(".task");
+let taskDragged;
+
+tasks.forEach((task) => task.addEventListener("dragstart", dragStart));
+
+taskContainers.forEach((taskContainer) => {
+  taskContainer.addEventListener("drop", dragDrop);
+  taskContainer.addEventListener("dragover", dragOver);
+});
+
+function dragStart(e) {
+  console.log(e.target);
+  taskDragged = e.target;
+}
+
+function dragDrop(e) {
+  console.log(e.target);
+  e.target.append(taskDragged);
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  // to stop the dragover
+}
